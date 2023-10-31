4bc401bd319a214f774d6e78939f7f1e0694f075
Jubair1996 <abujubair1996@gmail.com>
2023-10-31 21:18:31 +0600

    navbar active

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,13 +1,22 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../Pages/Navbar/Navbar";
+import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "../Pages/Footer/Footer";
+import Navbar from "../Pages/Navbar/Navbar";
+import Spinner from "../components/Spinner/Spinner";
 const MainLayout = () => {
+  const navigation = useNavigation();
+
   return (
     <div>
       <Navbar></Navbar>
-      <div className="min-h-screen">
-        <Outlet></Outlet>
-      </div>
+
+      {navigation.state === "loading" ? (
+        <Spinner></Spinner>
+      ) : (
+        <div className="min-h-screen">
+          <Outlet></Outlet>
+        </div>
+      )}
+
       <Footer></Footer>
     </div>
   );
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,15 +1,49 @@
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div className="flex justify-between items-center shadow-md py-5 px-12">
-            <h1 className="font-bold text-3xl">Amajhon</h1>
-            <nav className="flex items-center gap-8 py-4">
-                <a href="/">Home</a>
-                <a href="/products">Products</a>
-                <a href="/dashboard">Dashboard</a>
-            </nav>
-        </div>
-    );
+  return (
+    <div className="flex justify-between items-center shadow-md py-5 px-12">
+      <h1 className="font-bold text-3xl">Amajhon</h1>
+      <nav className="flex items-center gap-8 py-4">
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "bg-green-600 p-2 rounded "
+              : " "
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/products"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "bg-green-600 p-2 rounded "
+              : ""
+          }
+        >
+          Products
+        </NavLink>
+        <NavLink
+          to="/dashboard"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "bg-green-600 p-2 rounded"
+              : ""
+          }
+        >
+          Dashboard
+        </NavLink>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
