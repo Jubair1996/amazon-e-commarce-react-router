061c35550d2b0fd15535427a2888b8bd508656f9
Jubair1996 <abujubair1996@gmail.com>
2023-10-31 17:20:22 +0600

    load data

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,8 +1,14 @@
+import { useLoaderData } from "react-router-dom";
+import Product from "./Product";
 
 const Products = () => {
+    const {products} = useLoaderData();
+    console.log(products)
     return (
-        <div>
-            <h1>Products</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-12">
+            {
+                products.map(product => <Product key={product.id} product={product}></Product> )
+            }
         </div>
     );
 };
